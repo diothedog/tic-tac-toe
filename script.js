7486ad4b31f8d24767bba6efce62313204b47ead
diothedog <88989273+diothedog@users.noreply.github.com>
2024-07-09 15:23:13 -0500

    Get game working in console

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ const gameBoard = (function () {
 
     const getBoard = () => board;
 
-    return { getBoard };  
+    const printBoard = () => {
+        console.log(board);
+    }
+
+    return { getBoard, printBoard };  
 })();
 
 function createPlayer(name) {
@@ -15,3 +19,44 @@ function createPlayer(name) {
 
     return { players };
 }
+
+const gameController = (function() {
+    let board = gameBoard.getBoard();
+
+    const playerOne = prompt("Player One, what is your name?");
+    createPlayer(playerOne);
+    const playerTwo = prompt("Player Two, what is your name?");
+    createPlayer(playerTwo);
+
+    let activePlayer = playerOne;
+    let mark = "X"
+
+    const switchTurn = () => {
+        if (activePlayer === playerOne) {
+            activePlayer = playerTwo;
+            mark = "O";
+        } else {
+            activePlayer = playerOne;
+            mark = "X";
+        }
+    }
+
+    const playRound = () => {     
+        gameBoard.printBoard();
+
+        let cell = prompt(`${activePlayer}, where do you want to place your mark?`)
+        let index = Number(cell);
+
+        while (board[index] !== "") {
+            cell = prompt("That space is already occupied. Please select another space.");
+            index = Number(cell);
+        } 
+
+        board[index] = mark;
+        
+        switchTurn();
+    }
+
+    return { playRound };
+})();
+
